ab079bf6b44dcd8af41c35834b937af30fc8f623
pver <peetvermeulen@gmail.com>
2015-06-23 20:41:46 +0200

    Initial project setup: based on fsprojects/ProjectScaffold

---
+++ src/SportsDataTracker/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("SportsDataTracker")>]
+[<assembly: AssemblyProductAttribute("SportsDataTracker")>]
+[<assembly: AssemblyDescriptionAttribute("An application and library that allows analyzing sports data.")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
